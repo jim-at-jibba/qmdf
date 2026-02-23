36622b895279a85c19cf02a01536e9c1fb9745e7
James Best <jim@justjibba.net>
2026-02-23 06:05:49 +0000

    fix: read version from Go module info for go install builds

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/jim-at-jibba/qmdf/cmd"
+import (
+	"runtime/debug"
+
+	"github.com/jim-at-jibba/qmdf/cmd"
+)
 
 // version is set at build time via -ldflags "-X main.version=<tag>".
+// When installed via `go install ...@v0.1.0`, Go embeds the module version
+// in debug.BuildInfo, which we read as a fallback.
 var version = "dev"
 
 func main() {
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
+	}
 	cmd.Execute(version)
 }
